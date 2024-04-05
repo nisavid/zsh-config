53751b2c0f4f2ced95e923a768276a4f1898f4c9
Ivan D Vasin <ivan@nisavid.io>
2024-04-05 13:18:01 -0400

    fix(FZF_DEFAULT_OPTS): set colors only if terminal supports truecolor

--- zshrc.zsh
+++ zshrc.zsh
@@ -64,10 +64,11 @@ export BROWSER=firefox
 export SYSTEMD_EDITOR=$EDITOR
 
 export FZF_BASE=/usr/share/fzf
-export FZF_DEFAULT_OPTS=" \
-  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-  --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+[[ $COLORTERM = *(24bit|truecolor)* ]] \
+  && export FZF_DEFAULT_OPTS=" \
+    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+    --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
 if [[ -d ~/.gnupg ]] && (( $+commands[gpg-connect-agent] )); then
   export GPG_TTY=/dev/${(%):-%l}
