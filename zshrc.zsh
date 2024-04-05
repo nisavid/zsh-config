e99c05908f25b2c7831573c33a20b61af9d6c10f
Ivan D Vasin <ivan@nisavid.io>
2024-04-04 20:18:04 -0400

    feat(BROWSER): change to firefox

--- zshrc.zsh
+++ zshrc.zsh
@@ -57,7 +57,7 @@ export GROFF_NO_SGR=1
 export MANROFFOPT='-c'
 export MANWIDTH=$COLUMNS
 
-export BROWSER=vivaldi
+export BROWSER=firefox
 
 (( $+commands[nvim] )) && export EDITOR=nvim
 export SYSTEMD_EDITOR=$EDITOR
