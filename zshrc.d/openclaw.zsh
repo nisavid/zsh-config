c85b9bc429703250f1541d5373105a7cde72b210
Ivan D Vasin <ivan@nisavid.io>
2026-04-05 18:09:14 +0000

    feat(openclaw/aliases): split `clawup` into phases

--- zshrc.d/openclaw.zsh
+++ zshrc.d/openclaw.zsh
@@ -10,7 +10,10 @@ export OPENCLAW_NO_RESPAWN=1
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 
 alias cdoc='cd ~/.openclaw/workspace'
-#alias clawup='sudo apt update && sudo apt full-upgrade -y && pnpm self-update && pnpm -g up && pnpm -g approve-builds && clawhub update --all && openclaw gateway restart && openclaw doctor --fix'
-alias clawup='sudo apt update && sudo apt full-upgrade -y && brew update && brew upgrade && pnpm self-update && pnpm -g up && pnpm -g approve-builds && openclaw update && openclaw doctor --fix'
+alias clawup='clawup-pre && clawup-self && clawup-post && clawup-restart'
+alias clawup-pre='sudo apt update && sudo apt full-upgrade -y && brew update && brew upgrade && brew cleanup --prune=all && pnpm self-update && bun upgrade'
+alias clawup-post='uv tool upgrade --all && pnpm --global upgrade --latest && pnpm --global approve-builds && skills update && openclaw skills update --all'
+alias clawup-restart='openclaw gateway restart && openclaw doctor --fix'
+alias clawup-self='openclaw update && openclaw gateway reinstall --force'
 alias voc='v ~/.openclaw/openclaw.json'
 
