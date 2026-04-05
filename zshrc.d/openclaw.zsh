cda6a5e2d30ff7616109141a643216ed3e47edc4
Ivan D Vasin <ivan@nisavid.io>
2026-04-05 18:07:13 +0000

    perf(openclaw): speed up CLI startup

--- zshrc.d/openclaw.zsh
+++ zshrc.d/openclaw.zsh
@@ -1,4 +1,12 @@
 
+function {
+  local openclaw_compile_cache=/var/tmp/openclaw-compile-cache
+  [[ -w $openclaw_compile_cache(#qN-/) ]] || return
+  export NODE_COMPILE_CACHE=$openclaw_compile_cache
+}
+
+export OPENCLAW_NO_RESPAWN=1
+
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 
 alias cdoc='cd ~/.openclaw/workspace'
