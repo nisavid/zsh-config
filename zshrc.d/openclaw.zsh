d72e9e663862e5555f38da514252aad042450cc4
Ivan D Vasin <ivan@nisavid.io>
2026-04-05 18:08:41 +0000

    feat(openclaw/aliases): add `voc`

--- zshrc.d/openclaw.zsh
+++ zshrc.d/openclaw.zsh
@@ -12,4 +12,5 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 alias cdoc='cd ~/.openclaw/workspace'
 #alias clawup='sudo apt update && sudo apt full-upgrade -y && pnpm self-update && pnpm -g up && pnpm -g approve-builds && clawhub update --all && openclaw gateway restart && openclaw doctor --fix'
 alias clawup='sudo apt update && sudo apt full-upgrade -y && brew update && brew upgrade && pnpm self-update && pnpm -g up && pnpm -g approve-builds && openclaw update && openclaw doctor --fix'
+alias voc='v ~/.openclaw/openclaw.json'
 
