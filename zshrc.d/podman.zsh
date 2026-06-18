7b36d86ee3932f640b9e3fbeb6d0d797cacd61c7
Ivan D Vasin <ivan@nisavid.io>
2026-06-18 16:24:29 -0400

    fix(podman): correct DOCKER_HOST for macOS and fix /tmp typo
    On macOS, Podman Desktop exposes a Docker-compatible socket at
    /var/run/docker.sock; the previous Linux rootless path
    ($XDG_RUNTIME_DIR/podman/podman.sock) resolved to a dead, mistyped /tpm path
    there and broke docker-compose. Branch by OS and use the correct socket per
    platform.

--- zshrc.d/podman.zsh
+++ zshrc.d/podman.zsh
@@ -1,2 +1,6 @@
-export DOCKER_HOST=unix://${XDG_RUNTIME_DIR:-/tpm}/podman/podman.sock
+if [[ $OSTYPE == darwin* ]]; then
+  export DOCKER_HOST="unix:///var/run/docker.sock"
+else
+  export DOCKER_HOST="unix://${XDG_RUNTIME_DIR:-/run/user/$UID}/podman/podman.sock"
+fi
 export DOCKER_BUILDKIT=0
