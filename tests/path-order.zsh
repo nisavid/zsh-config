a3d3a47050b6518ecdb491066d876a134410b1d7
Ivan D Vasin <ivan@nisavid.io>
2026-07-23 12:10:50 -0400

    fix(path): prioritize secret command shims

---
+++ tests/path-order.zsh
@@ -0,0 +1,52 @@
+#!/usr/bin/env -S zsh -f
+
+emulate -L zsh
+setopt errexit nounset pipefail
+
+readonly repo_root=${0:A:h:h}
+tmpdir=$(mktemp -d)
+readonly tmpdir
+trap 'rm -rf -- $tmpdir' EXIT
+
+function fail {
+  print -ru2 -- "path-order test failed: $1"
+  return 1
+}
+
+fixture_home=$tmpdir/home
+fixture_bin=$tmpdir/bin
+homebrew_prefix=$tmpdir/homebrew
+rustup_prefix=$tmpdir/rustup
+shim_dir=$fixture_home/.local/lib/secret-exec/bin
+mkdir -p -- $fixture_bin $homebrew_prefix/bin $rustup_prefix/bin $shim_dir
+
+print -rl -- \
+  '#!/usr/bin/env zsh' \
+  'case $* in' \
+  "  '--prefix rustup') print -r -- ${(q)rustup_prefix} ;;" \
+  "  '--prefix') print -r -- ${(q)homebrew_prefix} ;;" \
+  '  *) exit 64 ;;' \
+  'esac' >$fixture_bin/brew
+print -rl -- '#!/usr/bin/env zsh' 'exit 0' >$shim_dir/k9s
+print -rl -- '#!/usr/bin/env zsh' 'exit 0' >$homebrew_prefix/bin/k9s
+chmod +x $fixture_bin/brew $shim_dir/k9s $homebrew_prefix/bin/k9s
+
+path_setup=$tmpdir/path-setup.zsh
+typeset -a setup_lines
+while IFS= read -r line; do
+  [[ $line == '# TODO: move to lazy init' ]] && break
+  setup_lines+=($line)
+done <$repo_root/zshrc.zsh
+print -rl -- $setup_lines >$path_setup
+
+HOME=$fixture_home
+PATH=$shim_dir:$fixture_bin:/usr/bin:/bin
+source $path_setup
+rehash
+
+[[ $path[1] == $shim_dir ]] ||
+  fail 'the managed secret-exec shim directory must remain first on PATH'
+[[ ${commands[k9s]:A} == ${shim_dir:A}/k9s ]] ||
+  fail 'command lookup must prefer the managed k9s shim over Homebrew'
+
+print -r -- 'path-order checks passed'
